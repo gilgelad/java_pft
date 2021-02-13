7cdb8d7b3ea0c8ef88a00b66b2a3717f22bea2d2
slepchenko <slepchenko@a-3.ru>
2021-02-14 02:30:02 +0300

    Refactor with Delegates creation. Lesson 2.6

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,15 +1,21 @@
-package ru.stqa.pft.addressbook;
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.NewContactData;
 
 import java.util.concurrent.TimeUnit;
-import org.testng.annotations.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class NewUserCreationTest {
-  private WebDriver wd;
+public class ApplicationManager {
 
-  @BeforeMethod(alwaysRun = true)
-  public void setUp() throws Exception {
+
+  public WebDriver wd;
+
+  public void init() {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/index.php");
@@ -22,6 +28,65 @@ public class NewUserCreationTest {
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  public void returnToGroupPage() {
+    wd.findElement(By.linkText("group page")).click();
+  }
+
+  public void submitGroupCreation() {
+    wd.findElement(By.name("submit")).click();
+  }
+
+  public void fillGroupForm(GroupData groupData) {
+    wd.findElement(By.name("group_name")).click();
+    wd.findElement(By.name("group_name")).clear();
+    wd.findElement(By.name("group_name")).sendKeys(groupData.getGroupName());
+    wd.findElement(By.name("group_header")).click();
+    wd.findElement(By.name("group_header")).clear();
+    wd.findElement(By.name("group_header")).sendKeys(groupData.getGroupHeader());
+    wd.findElement(By.name("group_footer")).click();
+    wd.findElement(By.name("group_footer")).clear();
+    wd.findElement(By.name("group_footer")).sendKeys(groupData.getGroupFooter());
+  }
+
+  public void initGroupCreation() {
+    wd.findElement(By.name("new")).click();
+  }
+
+  public void gotoGroupPage() {
+    wd.findElement(By.linkText("groups")).click();
+  }
+
+  public void stop() {
+    wd.findElement(By.linkText("Logout")).click();
+    wd.quit();
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      wd.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  public void deleteSelectedGroups() {
+    wd.findElement(By.name("delete")).click();
+  }
+
+  public void selectGroup() {
+    wd.findElement(By.name("selected[]")).click();
   }
 
   public void fillAddNewContactForm(NewContactData newContactData) {
@@ -51,34 +116,15 @@ public class NewUserCreationTest {
     wd.findElement(By.name("email")).sendKeys(newContactData.getUserTestEmail());
   }
 
-  private void returnToHomePage() {
+  public void returnToHomePage() {
     wd.findElement(By.linkText("home page")).click();
   }
 
-  private void submitNewUserCreation() {
+  public void submitNewUserCreation() {
     wd.findElement(By.name("submit")).click();
   }
 
-  private void gotoContactsPageAndAddNewUser() {
+  public void gotoContactsPageAndAddNewUser() {
     wd.findElement(By.linkText("add new")).click();
   }
-
-  @Test
-  public void testContactCreation() throws Exception {
-    gotoContactsPageAndAddNewUser();
-    fillAddNewContactForm(new NewContactData("Иван", "Иванович", "Иванов",
-            "Happy", "Test Happy Company",
-            "РФ, г. Москва, ул.Счастливая, д. 18", "+79999999999",
-            "testmail@mail.ru"));
-    submitNewUserCreation();
-    returnToHomePage();
-  }
-
-  @AfterMethod(alwaysRun = true)
-  public void tearDown() throws Exception {
-    wd.findElement(By.linkText("Logout")).click();
-    wd.quit();
-  }
-
 }
-
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/NewUserCreationTest.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.NewContactData;
+
+public class NewUserCreationTest extends TestBase {
+
+  @Test
+  public void testContactCreation() throws Exception {
+    app.gotoContactsPageAndAddNewUser();
+    app.fillAddNewContactForm(new NewContactData("Иван", "Иванович", "Иванов",
+            "Happy", "Test Happy Company",
+            "РФ, г. Москва, ул.Счастливая, д. 18", "+79999999999",
+            "testmail@mail.ru"));
+    app.submitNewUserCreation();
+    app.returnToHomePage();
+  }
+}
+
