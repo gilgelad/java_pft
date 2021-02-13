a68bbaa78774ae44b58be38f46662bf4c61fb689
slepchenko <slepchenko@a-3.ru>
2021-02-14 01:23:13 +0300

    New class for NewUserCreationTest was added

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/NewUserCreationTest.java
@@ -0,0 +1,84 @@
+package ru.stqa.pft.addressbook;
+
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class NewUserCreationTest {
+  private WebDriver wd;
+
+  @BeforeMethod(alwaysRun = true)
+  public void setUp() throws Exception {
+    wd = new FirefoxDriver();
+    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    wd.get("http://localhost/addressbook/index.php");
+    login("admin", "secret");
+  }
+
+  private void login(String username, String password) {
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys(username);
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys(password);
+    wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  public void fillAddNewContactForm(NewContactData newContactData) {
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(newContactData.getUserName());
+    wd.findElement(By.name("middlename")).click();
+    wd.findElement(By.name("middlename")).clear();
+    wd.findElement(By.name("middlename")).sendKeys(newContactData.getUserMiddleName());
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(newContactData.getUserLastName());
+    wd.findElement(By.name("nickname")).click();
+    wd.findElement(By.name("nickname")).clear();
+    wd.findElement(By.name("nickname")).sendKeys(newContactData.getUserNickName());
+    wd.findElement(By.name("company")).click();
+    wd.findElement(By.name("company")).clear();
+    wd.findElement(By.name("company")).sendKeys(newContactData.getUserCompanyName());
+    wd.findElement(By.name("address")).click();
+    wd.findElement(By.name("address")).clear();
+    wd.findElement(By.name("address")).sendKeys(newContactData.getUserCompanyAddress());
+    wd.findElement(By.name("mobile")).click();
+    wd.findElement(By.name("mobile")).clear();
+    wd.findElement(By.name("mobile")).sendKeys(newContactData.getUserMobilePhone());
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(newContactData.getUserTestEmail());
+  }
+
+  private void returnToHomePage() {
+    wd.findElement(By.linkText("home page")).click();
+  }
+
+  private void submitNewUserCreation() {
+    wd.findElement(By.name("submit")).click();
+  }
+
+  private void gotoContactsPageAndAddNewUser() {
+    wd.findElement(By.linkText("add new")).click();
+  }
+
+  @Test
+  public void testContactCreation() throws Exception {
+    gotoContactsPageAndAddNewUser();
+    fillAddNewContactForm(new NewContactData("Иван", "Иванович", "Иванов",
+            "Happy", "Test Happy Company",
+            "РФ, г. Москва, ул.Счастливая, д. 18", "+79999999999",
+            "testmail@mail.ru"));
+    submitNewUserCreation();
+    returnToHomePage();
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void tearDown() throws Exception {
+    wd.findElement(By.linkText("Logout")).click();
+    wd.quit();
+  }
+
+}
+
